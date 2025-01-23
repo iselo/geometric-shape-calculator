b9ac5df89133bb59512a472d339a2c381cc10069
iselo <50623030+iselo@users.noreply.github.com.>
2025-01-23 15:44:47 +0200

    Add package-info file

---
+++ src/main/java/org/test/shapecalculator/calculator/package-info.java
@@ -0,0 +1,4 @@
+@CheckReturnValue
+package org.test.shapecalculator.calculator;
+
+import com.google.errorprone.annotations.CheckReturnValue;
---
+++ src/main/java/org/test/shapecalculator/package-info.java
@@ -0,0 +1,4 @@
+@CheckReturnValue
+package org.test.shapecalculator;
+
+import com.google.errorprone.annotations.CheckReturnValue;
